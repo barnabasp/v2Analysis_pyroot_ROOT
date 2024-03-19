95c157f1ec2ba2313ba1818bc163890023ad70e3
barnabasp <barnabas.porfy@gmail.com>
2024-03-19 16:38:56 +0100

    v2 analyses added

---
+++ example_cpp/analyzetree/v2_analysis.cc
@@ -0,0 +1,238 @@
+#define particle_tree_cxx
+#include "particle_tree.h"
+#include <iostream>
+#include <cmath>
+#include <TH2.h>
+#include <TF1.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+#include <TWebFile.h>
+#include <vector>
+#include <TGraph.h>
+#include <TGraphErrors.h>
+#include <TFitResult.h>
+#include <TFitResultPtr.h>
+
+#define NCENT 6
+#define NPT 18
+
+const double centLims[NCENT+1] = {0,10,20,30,40,60,100};
+const double pTLims[NPT+1] = {0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2.0};
+
+TH1D *phidist[NPT][NCENT];
+TH1D *phirawphidist[NPT][NCENT];
+TGraph *v2values[NCENT];
+TGraphErrors *v2errs[NCENT];
+using namespace std;
+
+int whichCent(int centr)
+{
+//if the interval is not 0 - 100
+//    if( centr > centLims[NCENT] ) return NCENT-1;
+//    if( centr < centLims[0] ) return 0;
+
+    int ibin=0;
+    while( centr > centLims[ibin+1] ) ibin++;
+    return ibin;
+}
+int whichpT(double pTval)
+{
+
+    int ibin=0;
+    if( pTval > pTLims[NPT] ) return NPT-1;
+    if( pTval < pTLims[0] ) return 0;
+    while( pTval > pTLims[ibin+1] ) ibin++;
+    return ibin;
+}
+
+// -----------------------------------------------------------------------
+
+
+int main(int argc, const char** argv)
+{
+    if(argc<2)
+    {
+        cerr << "Usage: " << argv[0] << " <input file name> <output file name> <max events=-1>" << endl;
+        return -1;
+    }
+    string infilename(argv[1]);
+    string outfilename(argv[2]);
+    cerr << "Writing to " << outfilename.c_str() << endl;
+    int Nmaxevt = -1;
+    if(argc>=4) Nmaxevt = atoi(argv[3]);
+    if(Nmaxevt<1) Nmaxevt = -1;
+
+    //CREATE A HISTOGRAM, AND FILL IT BY LOOPING THROUGH ALL EVENTS
+    TH1 *pTdist = new TH1F("pTdist","pT distribution",100,0,2);
+    TH1 *centdist = new TH1F("centdist", "Centrality distribution", 100, 0, 100);
+    TH1 *zvertexdist = new TH1F("zvertexdist", "z Vertex distribution", 80, -40, 40);
+    TH1 *reacpldist = new TH1F("reacplandist", "Reaction plane distribution", 100, -2.0, 2.0);
+    TH1 *v2val = new TH1F("v2val", "True v2 values", 1000, 0.0, 0.1);
+
+    for(int icent = 0; icent < NCENT; icent++)
+    {
+        v2values[icent] = new TGraph(NPT);
+        v2values[icent]->SetName(Form("CentGraph%i",icent));
+        v2errs[icent] = new TGraphErrors(NPT);
+        v2errs[icent]->SetName(Form("CentErrsGraph%i",icent));
+        for(int ipT = 0; ipT < NPT; ipT++)
+        {
+            phidist[ipT][icent] = new TH1D(Form("phidist_cent%i_pT%i",icent, ipT), "PhiDistribution;phi;Entries", 200, -M_PI/2.0,M_PI/2.0);
+            phirawphidist[ipT][icent] = new TH1D(Form("phiCalcRP_RPDist_cent%i_pT%i",icent, ipT), "cos(2*(Measured #varphi - RP)) Distribution; cos(2*(Measured #varphi - RP)); Entries", 200, -M_PI/2.0,M_PI/2.0);
+        }
+    }
+    
+
+    particle_tree p(infilename.c_str());
+    if(p.fChain) cerr << "Tree initialized" << endl;
+    else { cerr << "No tree found." << endl; return 1; }
+    long unsigned int Nevents = p.fChain->GetEntries();
+    if(Nmaxevt>0 && Nmaxevt<(int)Nevents) Nevents=Nmaxevt;
+    cerr << "Will run on " << Nevents << " events (out of " << p.fChain->GetEntries()  << ")." << endl;
+
+    for(long unsigned int ievent=0;ievent<Nevents;ievent++)
+    {
+        if(ievent>0&&ievent%1000==0) cerr << ".";
+        if(ievent>0&&ievent%10000==0) cerr << "Analyzing event #" << ievent << endl;
+        p.GetEntry(ievent);
+
+        zvertexdist->Fill(p.Zvertex);
+
+        //centrality determination
+        int cent = p.Centrality;
+        centdist->Fill(cent);
+        int centBin = whichCent(cent);
+
+        double reactionPlane = p.ReactionPlane;
+        reacpldist->Fill(reactionPlane);
+        //variables for reactionplanePhi
+        double sumofSin[NPT] = {0};
+        double sumofCos[NPT] = {0};
+
+        //LOOP THROUGH ALL PARTICLES OF THE GIVEN EVENT
+        for(int ipart=0;ipart<p.Ntracks;ipart++)
+        {
+            double pT = sqrt(p.px[ipart]*p.px[ipart]+p.py[ipart]*p.py[ipart]);
+            int pTBin = whichpT(pT);
+            pTdist->Fill(pT);
+
+            //calculating raw phi from py/px 
+            double rawPhi = std::atan2(p.py[ipart],p.px[ipart]);
+            double phi = rawPhi-reactionPlane;
+            while(phi > M_PI/2) phi -= M_PI;
+            while(phi < -M_PI/2) phi += M_PI;
+//            cerr << "phi = " <<phi << "for pt, cent" << pTBin << " " << centBin <<endl;
+            phidist[pTBin][centBin]->Fill(phi);
+
+
+            sumofSin[pTBin] += sin(2*rawPhi);
+            sumofCos[pTBin] += cos(2*rawPhi);
+        }
+        for(int ipT = 0; ipT < NPT; ipT++)
+        { 
+            double realPhi = atan2(sumofCos[ipT]*2,sumofSin[ipT]);
+            //while(realPhi > M_PI/2) realPhi -= M_PI;
+            //while(realPhi < -M_PI/2) realPhi += M_PI;
+            double phirpDiff = 2*(realPhi-reactionPlane);
+//            while(phirpDiff > M_PI/2) phirpDiff -= M_PI;
+//            while(phirpDiff < -M_PI/2) phirpDiff += M_PI;
+            double phiRawPhi = cos(phirpDiff);
+
+            phirawphidist[ipT][centBin]->Fill(phiRawPhi);
+        }
+    }
+    cerr << endl;
+
+    TF1* fitFunc = new TF1("fitFunc","[0]+[1]*cos(2*x)", -TMath::Pi()/2,  TMath::Pi()/2);
+    for(int ipT = 0; ipT < NPT; ipT++)
+    {
+        for(int icent = 0; icent < NCENT; icent++)
+        { 
+            cerr << "cent: " <<icent << "\tpt = "<<ipT<< endl;
+            TFitResultPtr r = phidist[ipT][icent]->Fit(fitFunc, "S");
+
+            // constant term
+            double A = r->Parameter(0);
+            // coefficient for 2 * cos(2 * x)
+            double B = r->Parameter(1);
+            // get errors
+            double AErr = r->ParError(0);
+            double BErr = r->ParError(1);
+            // get covariance
+            double ABCov = r->CovMatrix(1, 0);
+
+            // elliptic flow (v2) and correction via reaction plane resolution
+
+            double v2_raw = B/(2.0*A);
+            cout << "Raw v2 is: " << v2_raw << endl;
+            //sigm
+            double evPlaneRes = sqrt(abs(phirawphidist[ipT][icent]->GetMean()));
+            cout << "event plane resolution: "<< evPlaneRes << endl;
+
+            double v2_true = v2_raw/evPlaneRes;
+
+
+
+            // error estimation through error propagation
+            double v2ErrNonCorr = std::abs(B / A) * std::sqrt((AErr * AErr) / (A * A) + (BErr * BErr) / (B * B) - 2 * ABCov / (A * B));
+            double v2Err = std::abs(B / A) * std::sqrt((AErr * AErr) / (A * A) + (BErr * BErr) / (B * B) - 2 * ABCov / (A * B)) / evPlaneRes;
+            double v2_truerr = v2_true*v2Err;
+            cout << "true v2: " << v2_true << "+-" << v2_truerr<< endl;
+
+            /*
+            phidist[ipT][icent]->Fit(fitFunc,"n");
+            double a = fitFunc->GetParameter(0);
+            double b = fitFunc->GetParameter(1);
+            double aerr = fitFunc->GetParError(0);
+            double berr = fitFunc->GetParError(1);
+            double error = (aerr/a + berr/b);
+            */
+
+
+            v2val->Fill(v2_true);
+            v2values[icent]->SetPoint(icent,pTLims[ipT],v2_true);
+            v2errs[icent]->SetPoint(ipT,pTLims[ipT]+0.05,v2_true);
+            v2errs[icent]->SetPointError(ipT,0,v2_truerr);
+            
+        }
+    }
+
+    
+
+    //WRITE ALL HISTOGRAMS TO THE OUTPUT ROOT FILE
+    TFile *f = new TFile(outfilename.c_str(),"RECREATE");
+    if(!f->IsWritable()) cerr << "File " << outfilename.c_str() << " was not opened!" << endl;
+    else cerr << "Analysis done, writing histos to " << outfilename.c_str() << endl;
+    f->cd();
+
+
+    pTdist->Write();
+    centdist->Write();
+    zvertexdist->Write();
+    reacpldist->Write();
+    v2val->Write();
+    for(int icent = 0; icent < NCENT; icent++)
+    {
+        v2values[icent]->Write();
+        v2errs[icent]->Write();
+        for(int ipT = 0; ipT < NPT; ipT++)
+        {
+            phidist[ipT][icent]->Write();
+            phirawphidist[ipT][icent]->Write();
+        }
+    }
+    f->Write();
+    f->Close();
+
+  //BELOW ARE SOME EXAMPLES TO DRAW STUFF WITHOUT THE NEED TO LOOP THROUGH EVENTS
+  //p.fChain->Draw("ReactionPlane","ReactionPlane>-9000");
+  //c1->Print("figs/ReactionPlane.png");
+  //p.fChain->Draw("Centrality","Centrality>-9000");
+  //c1->Print("figs/Centrality.png");
+  //p.fChain->Draw("Zvertex","Zvertex>-9000");
+  //c1->Print("figs/Zvertex.png");
+  //p.fChain->Draw("Ntracks");
+  //c1->Print("figs/Ntracks.png");
+  return 0; 
+}
+// -----------------------------------------------------------------------
---
+++ example_py/v2_analysis.py
@@ -0,0 +1,210 @@
+import sys
+import os
+import math
+import ROOT
+import numpy as np
+
+import time
+
+
+
+NCENT = 6
+NPT = 18
+centLims = np.array([0,10,20,30,40,60,100])
+pTLims = np.array([0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2.0])
+
+phidist = [[ROOT.TH1D() for _ in range(NCENT)] for _ in range(NPT)]
+calcEventPlaneResDist = [[ROOT.TH1D() for _ in range(NCENT)] for _ in range(NPT)]
+v2values = [ROOT.TGraph() for _ in range(NCENT)]
+v2errs = [ROOT.TGraphErrors() for _ in range(NCENT)]
+
+def whichCent(centr):
+    ibin = np.searchsorted(centLims, centr, side='right') - 1
+    return ibin
+
+def whichpT(pTval):
+    ibin = 0
+    if pTval > pTLims[NPT]:
+        return NPT - 1
+    if pTval < pTLims[0]:
+        return 0
+    while pTval > pTLims[ibin + 1]:
+        ibin += 1
+    return ibin
+
+
+def main(argv):
+    t0 = time.time()
+#    if len(argv) < 3:
+#        print("Usage: {} <input file name> <output file name> <max events=-1>".format(argv[0]))
+#        return -1
+
+#    infilename = argv[1]
+#    outfilename = argv[2]
+    infilename = "data.root"
+    outfilename = "test.root"
+    print("Writing to", outfilename)
+
+    Nmaxevt = -1
+    if len(argv) >= 4:
+        Nmaxevt = int(argv[3])
+    if Nmaxevt < 1:
+        Nmaxevt = -1
+
+
+    ROOT.gStyle.SetOptFit(1111)
+
+    # Create a histogram to be filled by looping through all events
+    pTdist = ROOT.TH1F("pTdist", "pT distribution", 100, 0, 2)
+    centdist = ROOT.TH1F("centdist", "Centrality distribution", 100, 0, 100)
+    zvertexdist = ROOT.TH1F("zvertexdist", "z Vertex distribution", 80, -40, 40)
+    reacpldist = ROOT.TH1F("reacplandist", "Reaction plane distribution", 100, -2.0, 2.0)
+    v2val = ROOT.TH1F("v2val", "True v2 values", 1000, 0.0, 0.1)
+    PxNP = ROOT.TGraph("RxNP.csv","%lg %lg"," \\t,;")
+
+    for icent in range(NCENT):
+        v2values[icent] = ROOT.TGraph(NPT)
+        v2values[icent].SetName(f"CentGraph{icent}")
+        v2errs[icent] = ROOT.TGraphErrors(NPT)
+        v2errs[icent].SetName(f"CentErrsGraph{icent}")
+        for ipT in range(NPT):
+            phidist[ipT][icent] = ROOT.TH1D(f"phidist_cent{icent}_pT{ipT}", "PhiDistribution;phi;Entries", 200,
+                                             -np.pi / 2.0, np.pi / 2.0)
+            calcEventPlaneResDist[ipT][icent] = ROOT.TH1D(f"phiCalcRP_RPDist_cent{icent}_pT{ipT}",
+                                                   "cos(2*(Measured #varphi - RP)) Distribution; cos(2*(Measured #varphi - RP)); Entries",
+                                                   200, -np.pi / 2.0, np.pi / 2.0)
+
+    histFile = ROOT.TFile.Open(infilename, "READ")
+    tree = histFile.Get("particle_tree")
+    # Determine how many events to run on
+    Nevents = tree.GetEntries()
+    if 0 < Nmaxevt < Nevents:
+        Nevents = Nmaxevt
+    print("Will run on {} events (out of {}).".format(Nevents, tree.GetEntries()))
+
+    # Loop through events in the given dataset
+    for ievent in range(Nevents):
+      tree.GetEntry(ievent)
+      # Monitor progress through output
+      if ievent > 0 and ievent % 1000 == 0:
+          print(".", end="", flush=True)
+      if ievent > 0 and ievent % 10000 == 0:
+          print("Analyzing event #", ievent)
+
+      zvertexdist.Fill(tree.Zvertex)
+
+      cent = tree.Centrality
+      centdist.Fill(cent)
+      centBin = whichCent(cent)
+      correction = []
+      corrCentBin = 0
+      y = []
+      for i in range(PxNP.GetN()):
+          x = PxNP.GetPointX(i)
+          if(corrCentBin == whichCent(x)):
+              y.append(PxNP.GetPointY(i))
+          else:
+              correction.append(np.average(y))
+              y = []
+          corrCentBin = whichCent(x)
+
+
+      reactionPlane = tree.ReactionPlane
+      reacpldist.Fill(reactionPlane)
+
+      sumofSin = np.array(NPT*[0])
+      sumofCos = np.array(NPT*[0])
+
+      # Loop through all particles of the given event
+      for ipart in range(tree.Ntracks):
+          pT = math.sqrt(tree.px[ipart] * tree.px[ipart] + tree.py[ipart] * tree.py[ipart])
+          pTdist.Fill(pT)
+          pTBin = whichpT(pT)
+
+          #calculate azimuthal angle for given particle (in lab)
+          phi0 = math.atan2(tree.py[ipart], tree.px[ipart])
+          #calculate azimuthal angle for given particle (in reaction plane)
+          phi = phi0 - reactionPlane
+          # fix to [-pi / 2, pi / 2]
+          while phi > math.pi / 2:
+              phi -= math.pi
+          while phi < -math.pi / 2:
+              phi += math.pi
+          #fill to histogram
+          phidist[pTBin][centBin].Fill(phi)
+          #Manual way to calculate the Reaction plane resolution
+          sumofSin[pTBin] += math.sin(2 * phi0)
+          sumofCos[pTBin] += math.cos(2 * phi0)
+      for ipT in range(NPT):
+          #calculate eventplane value for all - manual way
+          phiCalcRP = math.atan2(sumofCos[ipT] * 2, sumofSin[ipT])
+          phiCalcRP_RP = 2 * (phiCalcRP - reactionPlane)
+          calcEventPlaneRes = math.cos(phiCalcRP_RP)
+          #fill in histogram
+          calcEventPlaneResDist[ipT][centBin].Fill(calcEventPlaneRes)
+    t1 = time.time()
+    total = t1-t0
+    print()
+    #define fit function for n = 2
+    fitFunc = ROOT.TF1("fitFunc","[0]+[1]*cos(2*x)", -math.pi/2,  math.pi/2)
+    for ipT in range(NPT):
+        for icent in range(NCENT): 
+            print("cent:", icent, "\tpt =", ipT)
+            #fit phi to get parameters for v2
+            #phidist[ipT][icent].Fit(fitFunc,"n")
+            fitPtr = ROOT.TFitResultPtr(phidist[ipT][icent].Fit(fitFunc, "SV"))
+            #get parameters, constant and coefficient
+            a = fitPtr.Parameter(0)
+            b = fitPtr.Parameter(1)
+            #get errors
+            aerr = fitPtr.ParError(0)
+            berr = fitPtr.ParError(1)
+            #get covariance between parameters
+            abcov = fitPtr.CovMatrix(1, 0)
+            #error propagation
+            v2error_raw = abs(b/2.0*a) * np.sqrt((aerr**2) / (a**2) + (berr**2) / (b**2) - 2 * abcov / (a*b))
+
+            v2_raw = b/(2.0*a)
+            print("Raw v2 is:", v2_raw)
+            #R
+            evPlaneRes = np.sqrt(abs(calcEventPlaneResDist[ipT][icent].GetMean()))
+            evPlaneRes = correction[icent]
+            print("event plane resolution:", evPlaneRes)
+
+            v2_true = v2_raw/evPlaneRes
+            v2error_true = v2error_raw / evPlaneRes
+
+            print("true v2:", v2_true, "+-", v2error_true)
+            v2val.Fill(v2_true)
+            v2values[icent].SetPoint(icent,pTLims[ipT],v2_true)
+            v2errs[icent].SetPoint(ipT,pTLims[ipT]+0.05,v2_true)
+            v2errs[icent].SetPointError(ipT,0,v2error_true)
+    t2 = time.time()
+    total_2 = t2-t1
+    print(total)
+    print(total_2)
+    # Write all histograms to the output ROOT file
+    f = ROOT.TFile(outfilename, "RECREATE")
+    if not f.IsWritable():
+        print("File", outfilename, "was not opened!")
+    else:
+        print("Analysis done, writing histos to", outfilename)
+    f.cd()
+    pTdist.Write()
+    centdist.Write()
+    zvertexdist.Write()
+    reacpldist.Write()
+    v2val.Write()
+    for icent in range(NCENT):
+        v2values[icent].Write()
+        v2errs[icent].Write()
+        for ipT in range(NPT):
+            phidist[ipT][icent].Write()
+            calcEventPlaneResDist[ipT][icent].Write()
+
+    f.Write()
+    f.Close()
+
+    return 0
+
+sys.exit(main(sys.argv))
